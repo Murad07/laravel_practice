d290d5ba829bf8986723519a3b4f1ec5307bcc37
Murad07 <murad.pi22@gmail.com>
2021-11-29 15:39:16 +0600

    Migration - create a database table using migration process

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,3 +7,10 @@
 <h4>- Create component command: php artisan make:component Header</h4>
 <h6>- - Component create Two file one inside Views/components. and another inside app/View/Components</h6>
 <h4>- middleware command: php artisan make:middleware ageCheck</h4>
+
+<h5>Practice Date: 11/29/21</h5>
+<h3>Migration Process</h3>
+<li>- php artisan make:migration create_test_table</li>
+<li>Run avobe command to create a database table daynamically. We will found the created file inside database folder.</li>
+<li>Update the file with our table field</li>
+<li>- php artisan migrate. run the command to crete the table on database.</li>
